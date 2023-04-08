61ab083c07c3cbae405c0282e9bb6d467d3b1542
Dolores <104917935+d010r3s@users.noreply.github.com>
2023-04-08 23:32:09 +0300

    arc fix
    arc fixed, now working with any phi1-2-s

--- balloon 1st step.py
+++ balloon 1st step.py
@@ -46,11 +46,10 @@ phi2 = Xv[4]
 l = x2 - x1
 alpha1 = 3*math.pi/2 - phi1
 alpha2 = 3*math.pi/2
-x01 = [x1, x2]
-y01 = [y1, y2]
+# x01 = [x1, x2]
+# y01 = [y1, y2]
 
 fig, ax = plt.subplots()
-
 arc_x = x2
 arc_y = y2+0.01
 arc_width = 0.02
@@ -62,11 +61,10 @@ arc = patches.Arc((arc_x, arc_y),
                                  arc_height,
                                  theta1=arc_theta1,
                                  theta2=arc_theta2)
-arc_radius = ((0.5 * arc_width)**2 + arc_height**2) / 2*arc_height
 ax.add_patch(arc)
 arc_x = x1
 arc_y = y1+0.01
-arc_theta1 = math.degrees(phi1)
+arc_theta1 = -math.degrees(abs(phi1))
 arc_theta2 = 270
 arc = patches.Arc((arc_x, arc_y),
                                  arc_width,
@@ -81,7 +79,7 @@ x01 = [x1, x2]
 y01 = [y1, y2]
 x02 = [x1+arc_height/2, x2-arc_height/2]
 # print(arc_radius)
-y02 = [y1 + 0.01 + (arc_height/2) * sin(phi2), y2 + 0.01 + (arc_height/2) * sin(phi1)]
+y02 = [y1 + 0.01 - 0.5 * arc_height * sin(phi1), y2 + 0.01 - 0.5 * arc_height * sin(phi2)]
 ax.plot(x01, y01)
 ax.plot(x02, y02)
 
